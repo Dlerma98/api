8b61cecd513ba802112cf4b6f1d61fe95302a4da
Dlerma98 <davidglerma1990@gmail.com>
2024-10-25 16:39:00 +0200

    Fin paso 3 Aplicando un Middleware que nos ayuda a mandar los errores en formato JSON en caso de no enviarse nos muestra el mensaje de error que hemos creado en formato JSON

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Middleware\AlwaysAcceptJson;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->prependToGroup('api', AlwaysAcceptJson::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
+
+        $exceptions->renderable(function(NotFoundHttpException $e, Request $request) {
+            if($request->wantsJson()) {
+                return response()->json(['message'=>'Object Not Found'], 404);
+            }
+        });
         //
     })->create();
